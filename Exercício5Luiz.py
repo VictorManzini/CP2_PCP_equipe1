4a6e4a00ad754030396f528f4b278fbfad43e5c9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-14 10:13:06 -0300

    Para para poder terminar em casa

--- Exercício5Luiz.py
+++ Exercício5Luiz.py
@@ -3,9 +3,9 @@ idade=int(input("Digite sua idade"))
 renda_mensal=float(input("Qual a sua renda mensal?"))
 valor_desejado=float(input("Qual valor deseja pegar de emprestimo?"))
 numero_parcelas=input("Qual numero de parcelas deseja? 3 até 24 ")
-valor_desejado_2
+valor_desejado_2=float(valor_desejado * 20)
 
-if idade > 18 and valor_desejado <= 20 * renda_mensal:
+if idade > 18 and valor_desejado_2 <= 20 * renda_mensal:
     print("Financiamento aprovado")
 else:
     print("Financiamento reprovado")
