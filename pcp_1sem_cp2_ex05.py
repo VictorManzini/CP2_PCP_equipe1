6c0f4f0ff84afeaa4490b1f7a3015bde33e1b864
Victor.Manzini <victor@robertomanzini.com.br>
2026-04-16 08:09:53 -0300

    Corrigido erro no ex05

--- pcp_1sem_cp2_ex05.py
+++ pcp_1sem_cp2_ex05.py
@@ -4,26 +4,24 @@ def financiamento_aprovado(idade, renda, valor):
     return idade >= 18 and valor <= 20 * renda
 
 def taxa_parcelas(parcelas):
-    if parcelas <= 6:
+    if parcelas < 3 or parcelas > 24:
+        print("Parcela inválida, digite o valor desejado entre 3 e 24 parcelas.")
+        exit()
+    elif parcelas >= 3 and parcelas <= 6:
         return 0.05
-    elif parcelas <= 12:
+    elif parcelas >= 7 and parcelas <= 12:
         return 0.08
-    elif parcelas <= 24:
+    elif parcelas >= 13 and parcelas <= 24:
         return 0.10
 
-def valor_parcela(valor, taxa, parcelas):
-    if parcelas <= 6:
-        return valor >= taxa * parcelas
-    elif parcelas <= 12:
-        return valor >= taxa * parcelas
-    elif parcelas <= 24:
-        return valor >= taxa * parcelas
+def calcular_parcela(valor, taxa, parcelas):
+    return valor * (taxa *(1 + taxa) ** parcelas) / ((1 + taxa) ** parcelas - 1)    
 
 def valor_total(parcela, parcelas):
     return parcela * parcelas
 
 def valor_juros(valor, valor_total):
-    return valor - valor_total
+    return valor_total - valor
 
 
 nome = input("Digite seu nome: ")
@@ -32,15 +30,13 @@ renda_mensal = float(input("Qual a sua renda mensal? "))
 valor_desejado = float(input("Qual valor deseja pegar de empréstimo? "))
 numero_parcelas = int(input("Qual número de parcelas deseja? (3 até 24) "))
 
-if idade >= 18 and valor_desejado <= 20 * renda_mensal:
-    print("Financiamento aprovado")
+if financiamento_aprovado(idade, renda_mensal, valor_desejado):
 
     taxa = taxa_parcelas(numero_parcelas)
-    parcela = valor_parcela(valor_desejado, taxa, numero_parcelas)
+    parcela = calcular_parcela(valor_desejado, taxa, numero_parcelas)
     total = valor_total(parcela, numero_parcelas)
     juros = valor_juros(valor_desejado, total)
-
-    print("\n--- Financiamento aprovado ---")
+    print("Financiamento aprovado")
     print(f"Cliente: {nome}")
     print(f"Valor financiado: R$ {valor_desejado:.2f}")
     print(f"Taxa de juros: {taxa * 100:.0f}% ao mês")
